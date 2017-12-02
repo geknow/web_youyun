0383c4ce680010dd5df710e17e0e33ac94683dbb
GEKnow <ch.shanchuan@gmail.com>
2017-12-02 14:03:13 +0800

    body

--- src/components/FileBody/index.jsx
+++ src/components/FileBody/index.jsx
@@ -46,10 +46,58 @@ class FileBody extends React.Component {
                 </div>
 
 
-
                 <div className="file-body-right">
-                    <div className="body-right-header">
-                        
+                    <div className="right-header-content">
+                        <div className="body-right-header">
+                            <div className="header-name">
+                                <span>我的文件</span>
+                            </div>
+                            <div className="header-tag">
+                                <ul>
+                                    <li><span><i className="fa fa-plus-square-o" aria-hidden="true"></i>&nbsp;新建文件夹</span></li>
+                                    <li><span><i className="fa fa-cloud-download" aria-hidden="true"></i>&nbsp;下载</span></li>
+                                    <li><span><i className="fa fa-trash" aria-hidden="true"></i>&nbsp;删除</span></li>
+                                    <li><span><i className="fa fa-share-alt" aria-hidden="true"></i>&nbsp;分享</span></li>
+                                    <li><span><i className="fa fa-upload" aria-hidden="true"></i>&nbsp;上传</span></li>
+                                </ul>
+                            </div>
+                            <div className="header-file-tag">
+                                <ul>
+                                    <li>全部文件</li>
+                                    <li>修改日期</li>
+                                    <li>文件大小</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="r_hr" />
+                    <div className="right-body-content">
+                        <div className="content">
+                            <ul className="row">
+                                <li>
+                                    <ul>
+                                        <li><span>我的资源</span></li>
+                                        <li><span>2017年10月6号 14:02</span></li>
+                                        <li><span>100.0M</span></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <ul>
+                                        <li><span>我的资源</span></li>
+                                        <li><span>2017年10月6号 14:02</span></li>
+                                        <li><span>-</span></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <ul>
+                                        <li><span>我的资源</span></li>
+                                        <li><span>2017年10月6号 14:02</span></li>
+                                        <li><span>100.0M</span></li>
+                                    </ul>
+                                </li>
+
+                            </ul>
+                        </div>
                     </div>
                 </div>
 
